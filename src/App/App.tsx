ca90e40cff0d229ec53b90e4ed8a5991fdd990e2
gourgan <gourgan.hicham@gmail.com>
2022-04-04 17:02:32 +0200

    Using class with jsx and events

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,29 +1,45 @@
-import React, {useState} from "react";
-import "./App.css";
+import React, { Component } from "react";
+import style from "./App.module.css";
 import Button from "./component/Button/Button";
 
-function App() {
-  const  [nbClick, setNbClick] = useState(0);
+interface I_AppProps {
+  AppName?: string;
+}
 
-  return (
-    <div className="App">
-      Hello : {nbClick}
-      <Button
-        text="Mon button"
-        bgColor="Grey"
-        clickEvent={(arg: any) => {
-          setNbClick(nbClick + 1);
-          console.log(nbClick);
-        }}
-        type="submit"
-        color="red"
-      />
-      <Button text="Mon button div" type="reset" style={{borderRadius:'3px'}}>
-        <div>Hello</div>
-      </Button>
-      <Button text="Mon button string">simple string</Button>
-    </div>
-  );
+interface I_AppState {
+  counter: number;
+  oneValue: string;
+}
+
+class App extends Component<I_AppProps, I_AppState> {
+  constructor(props: I_AppProps) {
+    super(props);
+    this.state = { counter: 0, oneValue: "Hello" };
+  }
+
+  componentDidMount(){
+    console.log('%c%s','font-size:24pt;color:green;font-weight:900;text-align:center', '⚠⚠ Le component est monté ⚠⚠');
+  }
+  componentDidUpdate(oldProps:I_AppProps, oldState:I_AppState){
+    
+    console.log('%c%s','font-size:24pt;color:red;font-weight:900;text-align:center', '============Updates================');
+    console.log('Props => ', oldProps, this.props);
+    console.log('States => ', oldState, this.state);
+  }
+  render(): React.ReactNode {
+    return (
+      <div className={style.App}>
+        Valeur du compteur {this.state.counter}
+        <Button
+          clickEvent={() => {
+            this.setState({ counter: this.state.counter + 1 });
+          }}
+        >
+          {this.state.oneValue}
+        </Button>
+      </div>
+    );
+  }
 }
 
 export default App;
