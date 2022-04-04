851cc91c38fe1b6f85a438268e5912800d573a54
gourgan <gourgan.hicham@gmail.com>
2022-04-04 15:03:47 +0200

    style add spread operator

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
-import './App.css';
-import Button from './component/Button/Button';
+import React from "react";
+import "./App.css";
+import Button from "./component/Button/Button";
 
 function App() {
   return (
     <div className="App">
-      <Button text="Mon button" bgColor="Grey"/>
-      <Button text="Mon button 2"/>
-      <Button text="Mon button 3"/>
+      <Button
+        text="Mon button"
+        bgColor="Grey"
+        clickEvent={(arg: any) => {
+          console.log(arg);
+        }}
+        type="submit"
+        color="red"
+      />
+      <Button text="Mon button div" type="reset" style={{borderRadius:'3px'}}>
+        <div>Hello</div>
+      </Button>
+      <Button text="Mon button string">simple string</Button>
     </div>
   );
 }
--- src/App/component/Button/Button.js
+++ src/App/component/Button/Button.js
@@ -1,9 +1,41 @@
-import React from 'react';
-import style from './Button.module.css';
-
+import React from "react";
+import style from "./Button.module.css";
+import PropTypes from "prop-types";
 function Button(props) {
-
-    return <button className={style.Button} style={{backgroundColor:props.bgColor}}>{props.text}</button>;
+  return (
+    <button
+      className={style.Button}
+      type={props.type}
+      style={{
+        ...props.style,
+        backgroundColor: props.bgColor,
+        color: props.color,
+      }}
+      onClick={(args) => {
+        props.clickEvent("hello");
+      }}
+    >
+      {props.text}
+      <br />
+      {props.children}
+    </button>
+  );
 }
-
+Button.protoTypes = {
+  text: PropTypes.string.isRequired,
+  type: PropTypes.string.isRequired,
+  clickEvent: PropTypes.func.isRequired,
+  bgColor: PropTypes.string.isRequired,
+  children: PropTypes.any.isRequired,
+  color: PropTypes.string.isRequired,
+  style: PropTypes.object,
+};
+Button.defaultProps = {
+  clickEvent: () => {
+    console.warn("Fonction non fournie");
+  },
+  bgColor: "cornflowerblue",
+  type: "button",
+  color: "white",
+};
 export default Button;
